47574a6c1770703312a779106366c364e848a8bf
TheWarHawk2005 <56003863+TheWarHawk2005@users.noreply.github.com>
2025-02-21 21:28:42 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("/service-worker.js")
+      .then(() => console.log("Service Worker Registered"));
+  }
+  
+
+var speedElement = document.querySelector('#speed')
+
+navigator.geolocation.watchPosition(
+    (position) => {
+      var result
+      if (position.coords.speed !== null) {
+        speedMph = position.coords.speed * 2.23694
+        result = Math.round(speedMph)
+      } else {
+        result = "Speed data not available.";
+      }
+      speedElement.textContent = result
+    },
+    (error) => {
+      console.error("Error getting position:", error);
+      speedElement.textContent = error
+    },
+    {
+      enableHighAccuracy: true, // Ensures better accuracy (may drain battery)
+      maximumAge: 0, // Do not use cached position
+      timeout: 10000, // Timeout after 10 seconds
+    }
+  );
+  
